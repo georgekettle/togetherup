55030e719e2069217489fed45bf8d4c9b612252d
george kettle <georgekettle.gk@gmail.com>
2022-02-12 13:08:13 +1100

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env == 'development'
+	Comment.destroy_all
+	Goal.destroy_all
+	User.destroy_all
+	Log.destroy_all
+end
+
+user_1 = User.create(username: 'test_user', email: 'test@test.com', password: 'secret')
+
+goals = Goal.create([
+								{ 
+									name: "Run 10km",
+									style: 'metric',
+									metric_name: 'km',
+									metric_goal: 10,
+									end_date: Date.today + 10,
+									public: true,
+									message_to_self: "I knew you could do it... ",
+									user: user_1
+								},
+								{ 
+									name: "Save $20k",
+									style: 'metric',
+									metric_name: 'dollars',
+									metric_goal: 20000,
+									end_date: Date.today.next_year,
+									public: false,
+									message_to_self: "Omg... $20k you weapon! haha",
+									user: user_1
+								},
+								{ 
+									name: "Workout 3x per week",
+									style: 'frequency',
+									frequency_count: 3,
+									frequency_interval: :week,
+									frequency_slack: 100,
+									end_date: Date.today.next_month,
+									public: true,
+									message_to_self: "Yessss, nice one man.",
+									user: user_1
+								}])
