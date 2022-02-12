37cb3bfef24b6f2d4a3f205855baeab900814f30
george kettle <georgekettle.gk@gmail.com>
2022-02-13 10:56:42 +1100

    select metric or frequency

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require("tailwindcss/colors")
 
 module.exports = {
   content: [
@@ -9,8 +10,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+        sans: ['Poly Sans', ...defaultTheme.fontFamily.sans],
+        display: ['Neue Bit', ...defaultTheme.fontFamily.sans]
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      'jonquil': '#f9c80eff',
+      'banana': '#ffecc2ff',
+      'orange': '#f86624ff',
+      'silk': '#fdd6c4ff',
+      'red': '#ea3546ff',
+      'pink': '#fbdadeff',
+      'purple': '#662e9bff',
+      'lavender': '#e1d0f1ff',
+      'skyblue': '#2dd8eeff',
+      'blizzardblue': '#E8F9FF',
     },
   },
   plugins: [
